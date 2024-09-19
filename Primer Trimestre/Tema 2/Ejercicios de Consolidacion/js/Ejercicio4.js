2b8b5d9c3c554d09d172fa7a50e7cbb5e297f459
Mario Iglesias Capa <marioib1@informatica.iesvalledeljerteplasencia.es>
2024-09-19 20:37:23 +0200

    Ejercicio 4 Corregido

--- Primer Trimestre/Tema 2/Ejercicios de Consolidacion/js/Ejercicio4.js
+++ Primer Trimestre/Tema 2/Ejercicios de Consolidacion/js/Ejercicio4.js
@@ -3,13 +3,19 @@ let cadena = prompt("Introduzca una cadena:");
 
 function analizarCadena(cadena) {
     //CUENTA EL NÚMERO DE CARACTERES DE LA CADENA
-    console.log(cadena.length);
+    console.log("Número de caracteres: " + cadena.length);
 
     //CUENTA EL NÚMERO DE VOCALES QUE HAY
-    console.log(cadena.search(/[aeiou]/));
+    console.log("Número de vocales : " + cadena.match(/[aeiou]/gi).length);
 
     //EMPIEZA POR 'A'
-    console.log(cadena.startsWith("A"));
+    let cadenaEmpiezaPor = cadena.startsWith("A");
+
+    if (cadenaEmpiezaPor == true) {
+        console.log("Empieza por A.");
+    } else {
+        console.log("No empieza por A.");
+    }
 }
 
 //LLAMO A LA FUNCIÓN analizarCadena
