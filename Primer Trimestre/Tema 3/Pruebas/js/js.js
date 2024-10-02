62ab2055828ad3264484cba06964a47e15ac8911
Mario Iglesias Capa <marioib1@informatica.iesvalledeljerteplasencia.es>
2024-10-02 20:18:34 +0200

    Hasta la prueba 7

--- Primer Trimestre/Tema 3/Pruebas/js/js.js
+++ Primer Trimestre/Tema 3/Pruebas/js/js.js
@@ -70,3 +70,48 @@ history.go(2);
 //console.log(document.getElementById("titulo1")); // MUESTRA null. Selecciona un elemento del documento por medio del valor del atributo id que se le haya asignado.
 //console.log(document.getElementsByClassName("parrafos")); // MUESTRA HTMLCollection []. Devuelve elementos con la clase dada
 //console.log(document.getElementsByName("html tag")); // MUESTRA NodeList []. Inicia la búsqueda en un elemento específico dentro del árbol DOM
+
+// PRUEBA 6.1
+/*
+let head = document.head;
+
+//console.log(head.childNodes); // Devuelve una colección de hijos nodes del elemento dado donde el primer nodo hijo es asignado un índice 0.
+//console.log(head.children); // Contiene todos los elementos secundarios del elemento en el que se llamó.
+//console.log(head.parentNode); // Devuelve el padre del nodo especificado en el árbol.
+//console.log(head.nextSibling); // Devuelve el siguiente nodo con respecto al indicado en la lista de nodos a la que este pertenece o null si el nodo especificado es el último en dicha lista.
+//console.log(head.previousSibling); // Devuelve el contenido HTML del hermano anterior de un elemento
+//console.log(head.nextElementSibling); // Devuelve el elemento posterior al especificado, dentro de la lista de elementos hijos de su padre, o bien null si el elemento especificado es el último en dicha lista.
+//console.log(head.previousElementSibling); // Retorna el elemento predecesor al especificado dentro de la lista de hijos de su padre, o bien null si el elemento especificado es el primero de dicha lista.
+*/
+// PRUEBA 6.2
+/*
+let nodoBody = document.createElement("p");
+let textoNodo = document.createTextNode("Este es el párrafo final");
+
+nodoBody.appendChild(textoNodo);
+document.body.appendChild(nodoBody);
+*/
+// PRUEBA 7
+// 1)
+let elementosP = document.getElementsByTagName("p");
+
+console.log(elementosP);
+console.log("#############################################");
+
+// 2)
+for (var i = 0; i < elementosP.length; i++) {
+    console.log(elementosP[i]);
+}
+console.log("#############################################");
+
+// 3)
+for (var i = 0; i < elementosP.length; i++) {
+    if (elementosP[i].getElementsByTagName("parrafos")) {
+        elementosP[i].remove();
+    } 
+}
+
+// 4)
+for (var i = 0; i < elementosP.length; i++) {
+    console.log(elementosP[i]);
+}
