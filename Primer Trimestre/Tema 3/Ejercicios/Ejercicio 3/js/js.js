42ae67f1ceab38825afde77673b3016809bc0ac6
Mario Iglesias Capa <marioib1@informatica.iesvalledeljerteplasencia.es>
2024-10-03 20:46:27 +0200

    Ejercicio 3 Terminado

--- Primer Trimestre/Tema 3/Ejercicios/Ejercicio 3/js/js.js
+++ Primer Trimestre/Tema 3/Ejercicios/Ejercicio 3/js/js.js
@@ -1,9 +1,24 @@
-// 1
+function cambiarATwitter() {
+    
+    //1)
+    let nodoA = document.querySelector('a');
 
-// 2
+    //2)
+    nodoA.id = 'aTwitter';
 
-// 3
+    //3)
+    nodoA.href = 'https://www.twitter.com';
 
-// 4
+    //4)
+    nodoA.textContent = 'Twitter';
 
-// 5
+
+    //5) 
+    if (nodoA.hasAttribute("title")) {
+        nodoA.setAttribute("title", "Ir a Twitter")
+    } else {
+        console.log("No se cambia el atributo 'title' por que no tiene.");
+    }
+}
+
+cambiarATwitter();
