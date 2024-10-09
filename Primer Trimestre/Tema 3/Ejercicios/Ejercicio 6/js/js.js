477895bdb97e09a7b93177c96ade2101c73be6e1
Mario Iglesias Capa <marioib1@informatica.iesvalledeljerteplasencia.es>
2024-10-09 19:22:58 +0200

    Ejercicio 6 Terminado

--- Primer Trimestre/Tema 3/Ejercicios/Ejercicio 6/js/js.js
+++ Primer Trimestre/Tema 3/Ejercicios/Ejercicio 6/js/js.js
@@ -1,3 +1,4 @@
+/*
 document.addEventListener("DOMContentLoaded", main);
 
 function main() {
@@ -11,5 +12,67 @@ function main() {
     document.getElementById("divAzul").addEventListener(
         "click",
         (evento)=> {console.log("Pulsaste el divAzul")}
+    );
+}
+*/
+/*
+document.addEventListener("DOMContentLoaded", main);
+
+function main() {
+    
+    // Añadiremos los lsiteners:
+    document.getElementById("divRojo").addEventListener(
+        "click",
+        (evento)=> {console.log("Pulsaste el divRojo")},
+        true // EJERCICIO 6 MODIFICACIÓN
+    );
+
+    document.getElementById("divAzul").addEventListener(
+        "click",
+        (evento)=> {console.log("Pulsaste el divAzul")}
+    );
+}
+*/
+// PARA EL AZUL
+/*
+document.addEventListener("DOMContentLoaded", main);
+
+function main() {
+    
+    // Añadiremos los lsiteners:
+    document.getElementById("divRojo").addEventListener(
+        "click",
+        (evento)=> {console.log("Pulsaste el divRojo")
+        }
+    );
+
+    document.getElementById("divAzul").addEventListener(
+        "click",
+        (evento)=> {
+            console.log("Pulsaste el divAzul");
+            evento.stopPropagation()
+        }
+    );
+}
+*/
+// PARA EL ROJO
+document.addEventListener("DOMContentLoaded", main);
+
+function main() {
+    
+    // Añadiremos los lsiteners:
+    document.getElementById("divRojo").addEventListener(
+        "click",
+        (evento)=> {
+            console.log("Pulsaste el divRojo");
+            evento.stopPropagation()
+        },
+        true
+    );
+
+    document.getElementById("divAzul").addEventListener(
+        "click",
+        (evento)=> {
+            console.log("Pulsaste el divAzul")}
     );
 }
