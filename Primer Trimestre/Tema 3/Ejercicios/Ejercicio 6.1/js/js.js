49ff33cb4dfe843782c79bcc773bb1c1d62c5db4
Mario Iglesias Capa <marioib1@informatica.iesvalledeljerteplasencia.es>
2024-10-09 20:57:09 +0200

    Ejercicio 6.1 casi terminado

---
+++ Primer Trimestre/Tema 3/Ejercicios/Ejercicio 6.1/js/js.js
@@ -0,0 +1,67 @@
+let contenedorGeneral = document.getElementById("contenedorGeneral");
+
+//FUNCIÓN PARA CREAR LOS DIV
+function creaCaja() {
+    let caja = document.createElement("button");
+
+    caja.style.width = "25px";
+    caja.style.height = "25px";
+    caja.style.margin = "10px";
+    caja.style.backgroundColor = "black";
+
+    return caja;
+}
+
+// FUNCIÓN PARA CAMBIAR EL COLOR
+function cambiarColor(boton) {
+    if (boton.button === 0) {
+        switch (boton.target.style.backgroundColor) {
+            case "red":
+                boton.target.style.backgroundColor = "blue";
+                break;
+            case "blue":
+                boton.target.style.backgroundColor = "green";
+                break;
+            case "green":
+                boton.target.style.backgroundColor = "yellow";
+                break;
+            default:
+                boton.target.style.backgroundColor = "red";
+                break;
+        }
+    } else {
+        if (boton.button === 2) {
+            switch (boton.target.style.backgroundColor) {
+                case "red":
+                    boton.target.style.backgroundColor = "yellow";
+                    break;
+                case "yellow":
+                    boton.target.style.backgroundColor = "green";
+                    break;
+                case "green":
+                    boton.target.style.backgroundColor = "blue";
+                    break;
+                default:
+                    boton.target.style.backgroundColor = "red";
+                    break;
+            }
+            boton.preventDefault();
+        } else {
+            boton.target.style.backgroundColor = "gray";
+        }
+    }
+}
+
+//CREO LA MATRIZ DE 10 X 10
+for (let index = 0; index < 10; index++) {
+    for (let index = 0; index < 10; index++) {
+        let caja = creaCaja();
+
+        caja.addEventListener("auxclick", cambiarColor);
+        caja.addEventListener("click", cambiarColor);
+
+        contenedorGeneral.appendChild(caja);
+    }
+
+    contenedorGeneral.appendChild(document.createElement("br")); // SALTO DE LÍNEA
+}
