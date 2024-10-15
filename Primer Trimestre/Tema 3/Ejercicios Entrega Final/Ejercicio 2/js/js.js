4572d9e316e4f79e7c3f318268bab77338965618
Mario Iglesias Capa <marioib1@informatica.iesvalledeljerteplsencia.esm>
2024-10-15 11:49:41 +0200

    Ejercicio 2 Termiando

--- Primer Trimestre/Tema 3/Ejercicios Entrega Final/Ejercicio 2/js/js.js
+++ Primer Trimestre/Tema 3/Ejercicios Entrega Final/Ejercicio 2/js/js.js
@@ -3,21 +3,48 @@ let entradaTextoNota = document.getElementById("nota");
 
 let botonGuardarNota = document.getElementById("botonGuardarNota");
 
-elementoP = document.querySelector("p");
-
 // FUNCIÓN PARA CREAR UNA NOTA
 function crearaNota() {
-    let divNota = document.createElement("p");
+
+    // COMPRUEBO SI LA ENTRADA DE TEXTO ESTÁ VACÍA
+    if (entradaTextoNota.value === "") {
+        alert("Introduce el nombre de una nota.");
+        return;
+    }
+
+    let pNota = document.createElement("p");
     let nombreNota =  document.createTextNode(entradaTextoNota.value);
 
-    divNota.appendChild(nombreNota);
-    document.body.appendChild(divNota);
+    pNota.appendChild(nombreNota);
+    document.body.appendChild(pNota);
 
+    // DOY FUNCIONALIDAD A LA NOTA
+    pNota.addEventListener("click", function (evento) {
+        
+        // SI LA TECLA (Alt) ESTÁ PRESIONADA CAMBIO EL COLOR, SI NO LA BORRO
+        if (evento.altKey) {
+            cambiarColorDeaNota(pNota);
+        } else {
+            borrarNota(pNota);
+        }
+    });
+
+    entradaTextoNota.value = "";
+    entradaTextoNota.focus();
 }
 
 // FUNCIÓN PARA BORRAR UNA NOTA
-function crearaNota() {
+function borrarNota(nota) {
+    document.body.removeChild(nota);
+}
 
+// FUNCIÓN PARA CAMBIAR EL COLOR DE UNA NOTA
+function cambiarColorDeaNota(nota) {
+    if (nota.style.backgroundColor !== "red") {
+        nota.style.backgroundColor = "red";
+    } else {
+        nota.style.backgroundColor = "";
+    }
 }
 
 botonGuardarNota.addEventListener("click", crearaNota);
